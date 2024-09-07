8dc1b4ac6d79ce042979f55f60918179422f6d69
Anukul Raj <anukulraj2004@gmail.com>
2024-09-07 22:57:12 +0530

    Saas-Project-Setup-Done

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUP';
+import Home from '../pages/Home';
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/signin" element={<SignIn />} />
+			<Route path="/signup" element={<SignUp />} />
+		</Routes>
+	);
+};
+
+export default AppRoutes;
